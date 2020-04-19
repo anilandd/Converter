27a639a1bdcd7762ed2e78681f659e1a6cd29ef8
Anastasia Rudenko <aniland.send@gmail.com>
2020-04-20 00:04:45 +0300

    v 0.1 - without unit choice

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -9,8 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tempOrigin = "10"
+    
+    @State private var indexT = 0
+    let types = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    var tempConverted: Double {
+        let tOrigin = Double(tempOrigin) ?? 0
+        let res = tOrigin * 9/5 + 32
+        return res
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            
+            Form {
+                
+                Section {
+                    TextField("Temperature", text:  $tempOrigin)
+                }
+                
+                Section(header: Text("Input  unit")) {
+                    Picker("Type", selection: $indexT) {
+                        ForEach(0..<types.count) {
+                            Text("\(self.types[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Result")) {
+                    Text("\(tempConverted, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Result  unit")) {
+                     Picker("Type", selection: $indexT) {
+                         ForEach(0..<types.count) {
+                             Text("\(self.types[$0])")
+                         }
+                     }
+                     .pickerStyle(SegmentedPickerStyle())
+                 }
+                
+            }
+            .navigationBarTitle("Converter")
+            
+            
+        }
     }
 }
 

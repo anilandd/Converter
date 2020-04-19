96f7f12006bbc4e3cd0e782b11e3ccc1f87e2402
Anastasia Rudenko <aniland.send@gmail.com>
2020-04-19 23:39:44 +0300

    Initial Commit

---
+++ Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Converter
+//
+//  Created by Anastasia on 19.04.2020.
+//  Copyright © 2020 Anastasia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
